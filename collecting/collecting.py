5df73ec03b6b5b2b05905618ed1e8f3b6ddeb023
Alan Palazzolo <apalazzolo@minnpost.com>
2015-03-09 09:35:29 -0500

    Adding denver post.

--- collecting/collecting.py
+++ collecting/collecting.py
@@ -26,7 +26,7 @@ accounts = ['minnpost', 'nytimes', 'propublica', 'datadesk', 'texastribune',
     'sunlightlabs', 'inn', 'stlpublicradio', 'APMG', 'BBC-News-Labs',
     'ftlabs', 'Financial-Times', 'wraldata', 'theonion', 'jplusplus', 'datanews',
     'tarbell-project', 'newslynx', 'bvisualdata', 'bloomberg', 'seattletimes',
-    'newsdev']
+    'newsdev', 'denverpost']
 old_accounts = ['guardianinteractive']
 
 
